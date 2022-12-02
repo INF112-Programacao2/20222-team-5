637a0bfa68d462c54ce9e9f33a70dd623d9c6d78
Zopelar <viniciuszopelargon@gmail.com>
2022-12-02 14:26:53 -0300

    add classe base jogo

--- jogo/jogo.cpp
+++ jogo/jogo.cpp
@@ -3,5 +3,28 @@
 
 int Jogo::getApostaMinima()
 {
-  return apostaMinima;
+    return _apostaMinima;
+}
+
+std::string Jogo::getNome(){
+    return _nome;
+}
+
+Usuario Jogo::getUsuario(){ 
+}
+
+int Jogo::getValorApostado(){
+    return _valorApostado;
+}
+
+int Jogo::getPremiacao(){
+    return _premiacao;
+}
+
+void Jogo::setValorApostado(double valor){
+    _valorApostado = valor;
+}
+
+void Jogo::setPremiacao(double valor){
+    _premiacao = valor;
 }
--- jogo/jogo.h
+++ jogo/jogo.h
@@ -1,13 +1,28 @@
 #include <string>
+#include "usuario.h"
 
 class Jogo
 {
 private:
-  static int apostaMinima;
+  static int _apostaMinima;
+  static std::string _nome;
+  Usuario _user();
+  int _valorApostado;
+  int _premiacao;
+
 
 public:
-  static int getApostaMinima();
+  
   Jogo(std::string nome, int apostaMinima);
   Jogo();
   ~Jogo();
+
+  static int getApostaMinima();
+  static std::string getNome();
+  Usuario getUsuario();
+  int getValorApostado();
+  int getPremiacao();
+
+  void setValorApostado(double valor);
+  void setPremiacao(double valor);
 };
